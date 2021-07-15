0130a1c3c8c9fef1b5be7e8922d8b2ab0a1b9830
Кирило Овсяннiк <kovsiannik@e1r10p7.unit.ua>
2021-07-15 15:11:00 +0300

    add components

--- src/index.js
+++ src/index.js
@@ -1,48 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/app/';
 
 
 
-const Header = () => {
-  const scr = '<script>alert("Error!")</script>';
-  return <h2>Hello World!</h2>
-}
+// export default App;
 
-const Field = () => {
-  const styledField = {
-    width: '300px'
-  }
-  const holder = 'Enter here'
-  return <input style={styledField} type="text" placeholder={holder} autoComplete="" className="second" htmlFor=""/>
-}
-
-const Btn = () => {
-  const text = 'Log in';
-  const res = () => {
-    return 'Log in, please'
-  }
-  const logged = false;
-  const p = <p>Log in</p>
- return <button>{logged ? null : text}</button>
-}
-
-const NewApp = () => {
-  return (
-    <div>
-    <Header/>
-    <Field/>
-    <Btn/>
-    </div>
-  )
-}
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 
-ReactDOM.render(
-    <NewApp/>,
-  document.getElementById('root')
-);
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const Header = () => {
+  //   const scr = '<script>alert("Error!")</script>';
+  //   return <h2>Hello World!</h2>
+  // }
+  
+  // const Field = () => {
+  //   const styledField = {
+  //     width: '300px'
+  //   }
+  //   const holder = 'Enter here'
+  //   return <input style={styledField} type="text" placeholder={holder} autoComplete="" className="second" htmlFor=""/>
+  // }
+  
+  // const Btn = () => {
+  //   const text = 'Log in';
+  //   const res = () => {
+  //     return 'Log in, please'
+  //   }
+  //   const logged = false;
+  //   const p = <p>Log in</p>
+  //  return <button>{logged ? null : text}</button>
+  // }
+  
+  // const NewApp = () => {
+  //   return (
+  //     <div>
+  //     <Header/>
+  //     <Field/>
+  //     <Btn/>
+  //     </div>
+  //   )
+  // }
