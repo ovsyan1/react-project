7f0b60b9e049ac49c65dcfddd8769e567dfee96a
Кирило Овсяннiк <kovsiannik@e1r10p7.unit.ua>
2021-07-15 11:53:15 +0300

    first one

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="first">
+          Kyrylo <code>src/App.js</code> and save to reload.
+        </p>
+        <div>second block</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+
+const Header = () => {
+  const scr = '<script>alert("Error!")</script>';
+  return <h2>Hello World!</h2>
+}
+
+const Field = () => {
+  const styledField = {
+    width: '300px'
+  }
+  const holder = 'Enter here'
+  return <input style={styledField} type="text" placeholder={holder} autoComplete="" className="second" htmlFor=""/>
+}
+
+const Btn = () => {
+  const text = 'Log in';
+  const res = () => {
+    return 'Log in, please'
+  }
+  const logged = false;
+  const p = <p>Log in</p>
+ return <button>{logged ? null : text}</button>
+}
+
+const NewApp = () => {
+  return (
+    <div>
+    <Header/>
+    <Field/>
+    <Btn/>
+    </div>
+  )
+}
+
+
+ReactDOM.render(
+    <NewApp/>,
+  document.getElementById('root')
+);
+
+
